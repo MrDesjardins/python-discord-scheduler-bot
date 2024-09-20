89a827b69715cd828ab214351f6665c4f9d0ac00
Patrick Desjardins <mrdesjardins@gmail.com>
2024-09-20 14:42:04 -0700

    Better visualization with control on the thickness and showing user name

--- deps/analytic_visualizer.py
+++ deps/analytic_visualizer.py
@@ -1,6 +1,7 @@
 """
 Code to show the relationsip between the users
 """
+
 # pylint: disable=import-error
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -8,20 +9,30 @@ from analytic import cursor
 
 
 def display_graph_network_relationship() -> None:
+    """
+    Display the relationship between users in a graph
+    """
     # Fetch the data from the user_weights table
     cursor.execute(
         """
-  SELECT user_a, user_b, weight FROM user_weights
+    SELECT ui1.display_name as user_a_display_name, ui2.display_name as user_b_display_name, weight 
+    FROM user_weights 
+    left join user_info as ui1 on user_weights.user_a = ui1.id 
+    left join user_info as ui2 on user_weights.user_b = ui2.id
   """
     )
     data = cursor.fetchall()
+    # Extract weights for normalization
+    weights = [weight for _, _, weight in data]
+    max_weight = max(weights)
 
     # Create a graph using NetworkX
     G = nx.Graph()
 
     # Add edges with weights between users
-    for user_a, user_b, weight in data:
-        G.add_edge(user_a, user_b, weight=weight)
+    for user_a_display_name, user_b_display_name, weight in data:
+        normalized_weight = (weight / max_weight) * 100  # Normalize to max 100
+        G.add_edge(user_a_display_name, user_b_display_name, weight=normalized_weight)
 
     # Draw the graph
     plt.figure(figsize=(10, 10))
@@ -43,6 +54,6 @@ def display_graph_network_relationship() -> None:
     nx.draw_networkx_edge_labels(G, pos, edge_labels={k: f"{v:.2f}" for k, v in weights.items()})
 
     # Show plot
-    plt.title("User Relationship Graph (Edge Weight = Time Spent Together)")
+    plt.title("User Relationship Graph (Edge Tickness = More Time Together)")
     plt.axis("off")  # Turn off the axis
     plt.show()
