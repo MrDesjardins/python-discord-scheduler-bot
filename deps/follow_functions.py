1ade0a4fbfaa98f113091eee73cb53ad5edd95cc
Patrick Desjardins <mrdesjardins@gmail.com>
2025-12-23 10:37:59 -0800

    Avoid sending if the user is already in a voice channel

--- deps/follow_functions.py
+++ deps/follow_functions.py
@@ -1,6 +1,7 @@
-from deps.data_access import data_access_get_guild_voice_channel_ids, data_access_get_voice_user_list
+from deps.data_access import data_access_get_guild_voice_channel_ids, data_access_get_member, data_access_get_voice_user_list
 from deps.follow_data_access import fetch_all_followers_of_user_id
 from deps.mybot import MyBot
+from deps.log import print_error_log, print_log
 
 async def send_private_notification_following_user(bot: MyBot, joined_user_id: int, guild_id: int, channel_id: int):
     """Sends a private notification to users who are following the joined user."""
@@ -11,13 +12,22 @@ async def send_private_notification_following_user(bot: MyBot, joined_user_id: i
     if not followed_user_ids:
         return  # No one is following this user, nothing to do
 
-    # Filter down the list for only users who are not currently in an official voice channel
+    # Filter down the list for only users who are not currently in the same voice channel
     guild_voice_channels = await data_access_get_guild_voice_channel_ids(guild_id)
     if guild_voice_channels is None:
         guild_voice_channels = guild_voice_channels = await data_access_get_voice_user_list(guild_id, channel_id)
     filtered_followed_user_ids = [user_id for user_id in followed_user_ids if user_id not in guild_voice_channels]
 
-    for discord_user_id in filtered_followed_user_ids:
+    # Filter using Discord bot API to ensure is not in a voice channel
+    filtered2 = []
+    for user_id in filtered_followed_user_ids:
+        member = await data_access_get_member(guild, user_id)
+        if member and not member.voice:
+            filtered2.append(user_id)
+        else:
+            print_log(f"User {user_id} is currently in a voice channel; skipping notification for {joined_user_id}.")
+
+    for discord_user_id in filtered2:
         user = bot.get_user(discord_user_id)
         if user:
             try:
@@ -25,4 +35,4 @@ async def send_private_notification_following_user(bot: MyBot, joined_user_id: i
                     f"User <@{joined_user_id}> has joined <#{channel_id}> in **{guild.name}**."
                 )
             except Exception as e:
-                print(f"Failed to send DM to user {discord_user_id} for user {joined_user_id}: {e}")
+                print_error_log(f"Failed to send DM to user {discord_user_id} for user {joined_user_id}: {e}")
